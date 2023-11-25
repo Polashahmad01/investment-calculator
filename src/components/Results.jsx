88ae2e5588dcef89d0b28822acbec5f8f7c5c892
Polashahmad01 <dev.polashahmad@gmail.com>
2023-11-26 00:48:59 +0600

    Fix user input conversion and calculate investment results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,8 @@
+import { calculateInvestmentResults } from "../utils/investment"
+
 export default function Results({ input }) {
-  console.log(input)
+  const resultsData = calculateInvestmentResults(input)
+  console.log(resultsData)
 
   return (
     <p>Results...</p>
